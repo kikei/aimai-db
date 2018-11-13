9c05210698ae4d41a278eeb1f3547cc4826b10f1
kikei <fujii@xaxxi.net>
2018-11-14 01:41:10 +0900

    support partially fetching confidences and add show more button

--- client/src/components/ConfidencesView.js
+++ client/src/components/ConfidencesView.js
@@ -9,15 +9,27 @@ export default class ConfidencesView extends React.Component {
     this.state = {
       confidences: []
     }
+    this.clickShowMore = this.clickShowMore.bind(this)
   }
   componentDidMount() {
     console.log('ConfidencesView.componentDidMount')
-    this.getConfidences()
+    this.getConfidences({count:30})
   }
-  async getConfidences() {
+  clickShowMore(e) {
+    const confidences = this.state.confidences
+    console.log('showmore')
+    let before = null
+    if (confidences.length > 0)
+      before = confidences[confidences.length-1].timestamp
+    this.getConfidences({before:before, count:30})
+  }
+  async getConfidences(args) {
     const account = this.context
     const accountId = account.accountId
-    const uri = `/btctai/${accountId}/confidences`
+    const uri = new URL(`/btctai/${accountId}/confidences`, location.origin)
+    for (let prop in args)
+      if (args[prop] === null) delete args[prop]
+    uri.search = new URLSearchParams(args)
     console.log("Request confidences, uri:", uri)
     const opts = {
       method: "GET",
@@ -26,7 +38,7 @@ export default class ConfidencesView extends React.Component {
     try {
       const {response, json} = await fetchProtectedJSON(account, uri, opts)
       console.log("confidences fetched:", json)
-      this.setState({confidences: json})
+      this.setState({confidences: this.state.confidences.concat(json)})
     } catch (error) {
       console.error("Failed to get values:", error)
       return
@@ -68,6 +80,10 @@ export default class ConfidencesView extends React.Component {
                     }
                   </div>
                 </Table>
+                <div className="siimple-btn siimple-btn--primary siimple-btn--fluid"
+                     onClick={this.clickShowMore}>
+                  Show more
+                </div>
               </div>
             )
           }
