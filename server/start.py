c6668cbf7d46b2b5517fd05db0634f13bb516c99
kikei <fujii@xaxxi.net>
2018-11-14 01:49:10 +0900

    make api for fetching trades support filter parameters

--- server/start.py
+++ server/start.py
@@ -112,7 +112,16 @@ def getBtctaiTrades(accountId):
   identity = flask_jwt.get_jwt_identity()
   if accountId != identity:
     flask.abort(403)
-  trades = modelTrades.all(accountId=identity)
+  count = flask.request.args.get('count', None)
+  before = flask.request.args.get('before', None)
+  try:
+    if count is not None:
+      count = int(count)
+    if before is not None:
+      before = float(before)
+  except ValueError as e:
+    flask.abort(400)
+  trades = modelTrades.all(accountId=identity, before=before, count=count)
   trades = [t.toDict() for t in trades]
   return flask.jsonify(trades)
 
