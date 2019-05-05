c861d89ecaddbb384643618d83e2e158af5f58ee
kikei <fujii@xaxxi.net>
2019-05-06 08:37:05 +0900

    add TrendView.js

---
+++ client/src/components/TrendView.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { AccountContext } from '../contexts/contexts'
+import { fetchProtectedJSON } from '../utils'
+import { Table, Tr, Td } from '../components/tables'
+
+export default class TrendView extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      strength: []
+    }
+    this.clickShowMore = this.clickShowMore.bind(this)
+  }
+  componentDidMount() {
+    console.log('TrendView.componentDidMount')
+    this.getStrength({count:30})
+  }
+  clickShowMore(e) {
+    const strength = this.state.strength
+    let before = null
+    if (strength.length > 0)
+      before = strength[strength.length-1].timestamp
+    this.getStrength({before:before, count:30})
+  }
+  async getStrength(args) {
+    const account = this.context
+    const accountId = account.accountId
+    const uri = new URL(`/btctai/${accountId}/trendStrength`, location.origin)
+    for (let prop in args)
+      if (args[prop] === null) delete args[prop]
+    uri.search = new URLSearchParams(args)
+    console.log("Request trend strength, uri:", uri)
+    const opts = {
+      method: "GET",
+      enableRefreshToken: true
+    }
+    try {
+      const {response, json} = await fetchProtectedJSON(account, uri, opts)
+      console.log("trend strength fetched:", json)
+      this.setState({strength: this.state.strength.concat(json)})
+    } catch (error) {
+      console.error("Failed to get values:", error)
+      return
+    }
+  }
+  render() {
+    const state = this.state
+    return (
+      <AccountContext.Consumer>
+        {
+          (account) => {
+            console.log("TrendView.render, account:", account,
+                        "state:", state)
+            return (
+              <div className="siimple-content">
+                <h2>Trend</h2>
+                <Table>
+                  <div className="siimple-table-header">
+                    <Tr>
+                      <Td>Date</Td>
+                      <Td>Strength</Td>
+                    </Tr>
+                  </div>
+                  <div className="siimple-table-body">
+                    {
+                      state.strength.map((c, i) => {
+                        const date = new Date(c.timestamp * 1000)
+                        return (
+                          <Tr key={i}>
+                            <Td>{date.toLocaleString()}</Td>
+                            <Td>{c.strength.toFixed(3)} %</Td>
+                          </Tr>
+                        )
+                      })
+                    }
+                  </div>
+                </Table>
+                <div className="siimple-btn siimple-btn--primary siimple-btn--fluid"
+                     onClick={this.clickShowMore}>
+                  Show more
+                </div>
+              </div>
+            )
+          }
+        }
+      </AccountContext.Consumer>
+    )
+  }
+}
+TrendView.contextType = AccountContext;
